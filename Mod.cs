d7f976e39df01588e80caadb1875a2bb11e2bf57
lazy-red-panda-qw <2929954331@qq.com>
2025-06-14 03:16:17 +0800

    Add the original Structure

---
+++ Mod.cs
@@ -0,0 +1,27 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using System.IO;
+
+namespace AirportNext
+{
+    public class Mod : IMod
+    {
+        private string pathToModFolder;
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            if (!GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset)) return;
+
+            pathToModFolder = $"{new FileInfo(asset.path).DirectoryName}";
+
+            ExtraAssetsImporter.EAI.LoadCustomAssets(pathToModFolder);
+
+        }
+
+        public void OnDispose()
+        {
+        }
+    }
+}
