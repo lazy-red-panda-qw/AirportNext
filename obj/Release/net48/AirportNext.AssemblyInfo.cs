55b3bed74a8b4b3a019f17b3922f7e1c6a216c09
lazy-red-panda-qw <2929954331@qq.com>
2025-06-14 19:47:06 +0800

    small fix

--- obj/Release/net48/AirportNext.AssemblyInfo.cs
+++ obj/Release/net48/AirportNext.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirportNext")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f976e39df01588e80caadb1875a2bb11e2bf57")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirportNext")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirportNext")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
